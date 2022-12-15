b7e112223606c61ec5796c6d7510d95761f5a1c4
KIMSUJIN98 <qwer9402@naver.com>
2022-12-15 15:03:59 +0900

    kh_edu
    JDBC 2022-12-15

--- 99_JDBC_HomeWork/src/test/TestRun.java
+++ 99_JDBC_HomeWork/src/test/TestRun.java
@@ -11,180 +11,113 @@ import java.util.Scanner;
 public class TestRun {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		while(true) {
-			// 실행 메뉴 구현하기
-			System.out.println("============================");
-			System.out.println("PRODUCT 테이블에서 작업하기");
-			System.out.println("1. SELECT");
-			System.out.println("2. INSERT");
-			System.out.println("3. DELETE");
-			System.out.println("4. 종료");
-			System.out.println("============================");
-			System.out.print("수행할 번호 입력: ");
-			int menu = sc.nextInt();
-			sc.nextLine();
-			
-			Connection conn = null;		// DB의 연결정보를 보관할 객체
-			Statement stmt = null;		// sql문 전달해서 실행 후 결과 받는 객체
-			String sql = null;			// 앞으로 실행할 SQL문을 담을 변수
-			int result = 0;				// 결과(처리된 행수)를 받아줄 변수
-			
-			switch(menu) {
-			case 1:	// 1. SELECT
-				ResultSet rset = null; 		// select문 실행하여 조회된 결과값들이 처음에 실질적으로 담길 객체
-				
-				sql = "SELECT * FROM PRODUCT";
-				
-				try {
-					// 1) jdbc driver 등록
-					Class.forName("oracle.jdbc.driver.OracleDriver");
-				
-					// 2) Connection 객체 생성
-					conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
-				
-					// 3) Statement 객체 생성
-					stmt = conn.createStatement();
-					
-					// 4,5) sql문 전달해서 실행 후 결과 받기 (ResultSet 객체)
+
+		Connection conn = null; // DB의 연결정보를 보관할 객체
+		Statement stmt = null; // sql문 전달해서 실행 후 결과 받는 객체
+		ResultSet rset = null; // select문 실행하여 조회된 결과값들이 처음에 실질적으로 담길 객체
+		int result = 0; // 결과(처리된 행수)를 받아줄 변수
+		boolean dml = false;
+		String sql = null; // 앞으로 실행할 SQL문을 담을 변수
+		String type = null;
+
+		try {
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
+
+			stmt = conn.createStatement();
+
+			while (true) { // -- while 반복문 시작
+				System.out.println("============================");
+				System.out.println("PRODUCT 테이블에서 작업하기");
+				System.out.println("1. SELECT");
+				System.out.println("2. INSERT");
+				System.out.println("3. DELETE");
+				System.out.println("4. 종료");
+				System.out.println("============================");
+				System.out.print("수행할 번호 입력: ");
+				int menu = sc.nextInt();
+				sc.nextLine();
+
+				switch (menu) { // -- switch문 시작
+				case 1: // 1. SELECT
+					dml = false;
+
+					sql = "SELECT * FROM PRODUCT";
+
 					rset = stmt.executeQuery(sql);
-					
-					// 6)
-					while(rset.next()) {
+
+					while (rset.next()) {
 						int pno = rset.getInt("PNO");
 						String pname = rset.getString("PNAME");
 						int price = rset.getInt("PRICE");
 						Date reg_date = rset.getDate("REG_DATE");
-						
+
 						System.out.println(pno + ", " + pname + ", " + price + ", " + reg_date);
 					}
-					
-				} catch (ClassNotFoundException e) {
-					e.printStackTrace();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} finally {
-					try {
-						rset.close();
-						stmt.close();
-						conn.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}
-				
-				break;
-				
-			case 2:	// 2. INSERT
-				System.out.print("제품이름 : ");
-				String name = sc.nextLine();
-				
-				System.out.print("제품가격 : ");
-				int price = sc.nextInt();
-				sc.nextLine();
-				
-				sql = "INSERT INTO PRODUCT VALUES(SEQ_PNO.NEXTVAL, '" + name + "', " + price + ", SYSDATE)";
-				
-				try {
-					// 1) jdbc driver 등록
-					Class.forName("oracle.jdbc.driver.OracleDriver");
-					
-					// 2) Connection 객체 생성 : DB에 연결(url, 계정명, 비번)
-					conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
-					
-					// 3) Statement 객체 생성
-					stmt = conn.createStatement();
-					
-					// 4,5) sql문 전달하면서 실행 후 결과받기 (처리된 행수)
+					break;
+
+				case 2: // 2. INSERT
+					dml = true;
+					type = "INSERT";
+
+					System.out.print("제품이름 : ");
+					String name = sc.nextLine();
+
+					System.out.print("제품가격 : ");
+					int price = sc.nextInt();
+					sc.nextLine();
+
+					sql = "INSERT INTO PRODUCT VALUES(SEQ_PNO.NEXTVAL, '" + name + "', " + price + ", SYSDATE)";
+					break;
+
+				case 3: // 3. DELETE
+					dml = true;
+					type = "DELETE";
+
+					System.out.print("삭제할 제품번호 : ");
+					int num = sc.nextInt();
+					sc.nextLine();
+
+					sql = "DELETE FROM PRODUCT WHERE PNO =" + num;
+					break;
+
+				case 4: // 4. 종료
+					System.out.println("프로그램을 종료합니다.");
+					return;
+				} // -- switch문 종료
+
+				if (dml) {
 					result = stmt.executeUpdate(sql);
-					
-					// 6) 트랜젝션 처리
-					if(result > 0) { 		// 성공했을 경우 commit
+
+					if (result > 0) { // 성공했을 경우
 						conn.commit();
-					}else { 				// 실패했을 경우 rollback
+						System.out.printf("PRODUCT 테이블에서 정상적으로 %s 되었습니다.\n", type);
+					} else { // 실패했을 경우
 						conn.rollback();
-					}
-					
-				} catch (ClassNotFoundException e) {
-					e.printStackTrace();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} finally {
-					try {
-						// 7) 다 쓴 JDBC 객체 자원 반납 (생성된 역순으로)
-						stmt.close();
-						conn.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
+						System.out.printf("PRODUCT 테이블에서 %s 하지 못했습니다.\n", type);
 					}
 				}
-				
-				if(result > 0) {
-					System.out.println("PRODUCT 테이블에 정상적으로 INSERT 되었습니다.");
-				}else {
-					System.out.println("PRODUCT 테이블에 INSERT 하지 못했습니다.");
-				}
-				
-				break;
-				
-			case 3:	// 3. DELETE
-				System.out.print("삭제할 제품번호 : ");
-				int num = sc.nextInt();
-				sc.nextLine();
-				
-				sql = "DELETE FROM PRODUCT WHERE PNO =" + num;
-				
-				try {
-					// 1) jdbc driver 등록
-					Class.forName("oracle.jdbc.driver.OracleDriver");
-					
-					// 2) Connection 객체 생성 : DB에 연결(url, 계정명, 비번)
-					conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
-					
-					// 3) Statement 객체 생성
-					stmt = conn.createStatement();
-					
-					// 4,5) sql문 전달하면서 실행 후 결과받기 (처리된 행수)
-					result = stmt.executeUpdate(sql);
-					
-					// 6) 트랜젝션 처리
-					if(result > 0) { 		// 성공했을 경우 commit
-						conn.commit();
-					}else { 				// 실패했을 경우 rollback
-						conn.rollback();
-					}
-					
-				} catch (ClassNotFoundException e) {
-					e.printStackTrace();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} finally {
-					try {
-						// 7) 다 쓴 JDBC 객체 자원 반납 (생성된 역순으로)
-						stmt.close();
-						conn.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}
-				
-				if(result > 0) {
-					System.out.println("PRODUCT 테이블에서 정상적으로 DELETE 되었습니다.");
-				}else {
-					System.out.println("PRODUCT 테이블에서 DELETE 하지 못했습니다.");
-				}
-				
-				break;
-				
-			case 4:	// 4. 종료
-				System.out.println("프로그램을 종료합니다.");
-				return;
+
+			} // -- while 반복문 종료
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rset.close(); // rset을 전역으로 선언 => DML만 수행한 경우에도 닫아야함.
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (NullPointerException e) {
+				// SELECT없이 DML만 수행한 경우 예외처리 위함.
 			}
 		}
-		
-		
 	}
-
 }
